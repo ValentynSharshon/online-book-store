e14439b6af90e6281590c2219d96febc1962011d
Valentyn Sharshon <woosay333@gmail.com>
2024-04-18 15:59:44 +0300

    created DataProcessingException unchecked exception class

---
+++ src/main/java/com/gmail/woosay333/onlinebookstore/exception/DataProcessingException.java
@@ -0,0 +1,7 @@
+package com.gmail.woosay333.onlinebookstore.exception;
+
+public class DataProcessingException extends RuntimeException {
+    public DataProcessingException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
