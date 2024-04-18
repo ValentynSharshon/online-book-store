3bb4c986e34c1395397b896bbb120c8cecdb6e3b
Valentyn Sharshon <woosay333@gmail.com>
2024-04-18 15:09:13 +0300

    created Book entity class

---
+++ src/main/java/com/gmail/woosay333/onlinebookstore/entity/Book.java
@@ -0,0 +1,36 @@
+package com.gmail.woosay333.onlinebookstore.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "books")
+@Data
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String author;
+
+    @Column(unique = true, nullable = false)
+    private String isbn;
+
+    @Column(nullable = false)
+    private BigDecimal price;
+
+    private String description;
+
+    private String coverImage;
+}
