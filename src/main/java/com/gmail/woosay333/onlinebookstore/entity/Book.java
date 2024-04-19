4666f9aab3d3ef60dcb17aab70d91679fef14390
Valentyn Sharshon <woosay333@gmail.com>
2024-04-19 11:53:23 +0300

    changed @Data annotation to @Getter, @Setter, @EqualsAndHashCode, @ToString

--- src/main/java/com/gmail/woosay333/onlinebookstore/entity/Book.java
+++ src/main/java/com/gmail/woosay333/onlinebookstore/entity/Book.java
@@ -9,14 +9,20 @@ import jakarta.persistence.Table;
 import java.math.BigDecimal;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 @Entity
 @Table(name = "books")
-@Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
+@ToString
 @Builder
 public class Book {
     @Id
