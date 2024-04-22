d684993483f4f625620d9445d747e34c15eb3639
Valentyn Sharshon <woosay333@gmail.com>
2024-04-22 21:44:24 +0300

    added Optional<Book> findById(Long id) method

--- src/main/java/com/gmail/woosay333/onlinebookstore/repository/BookRepository.java
+++ src/main/java/com/gmail/woosay333/onlinebookstore/repository/BookRepository.java
@@ -2,9 +2,12 @@ package com.gmail.woosay333.onlinebookstore.repository;
 
 import com.gmail.woosay333.onlinebookstore.entity.Book;
 import java.util.List;
+import java.util.Optional;
 
 public interface BookRepository {
     Book save(Book book);
+
+    Optional<Book> findById(Long id);
 
     List<Book> findAll();
 }
