21d46721ecb7a751193be57ee6f8340d8987ef16
Valentyn Sharshon <woosay333@gmail.com>
2024-04-26 17:34:47 +0300

    added book update and delete methods

--- src/main/java/com/gmail/woosay333/onlinebookstore/controller/BookController.java
+++ src/main/java/com/gmail/woosay333/onlinebookstore/controller/BookController.java
@@ -1,13 +1,15 @@
 package com.gmail.woosay333.onlinebookstore.controller;
 
-import com.gmail.woosay333.onlinebookstore.dto.BookResponseDto;
+import com.gmail.woosay333.onlinebookstore.dto.BookDto;
 import com.gmail.woosay333.onlinebookstore.dto.CreateBookRequestDto;
 import com.gmail.woosay333.onlinebookstore.service.BookService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,17 +21,30 @@ public class BookController {
     private final BookService bookService;
 
     @GetMapping
-    public List<BookResponseDto> getAllBooks() {
+    public List<BookDto> getAllBooks() {
         return bookService.findAll();
     }
 
     @GetMapping("/{id}")
-    public BookResponseDto getBookById(@PathVariable Long id) {
+    public BookDto getBookById(@PathVariable Long id) {
         return bookService.findById(id);
     }
 
     @PostMapping
-    public BookResponseDto saveBook(@RequestBody CreateBookRequestDto bookRequestDto) {
+    public BookDto saveBook(@RequestBody CreateBookRequestDto bookRequestDto) {
         return bookService.save(bookRequestDto);
+    }
+
+    @PutMapping("/{id}")
+    public BookDto updateBook(
+            @PathVariable Long id,
+            @RequestBody CreateBookRequestDto bookRequestDto
+    ) {
+        return bookService.update(id, bookRequestDto);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        bookService.delete(id);
     }
 }
