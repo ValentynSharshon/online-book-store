26fba2107584b5c5b3d6c20b387f39426a66996a
Valentyn Sharshon <woosay333@gmail.com>
2024-04-30 21:08:52 +0300

    added inheritance from the JpaSpecificationExecutor interface

--- src/main/java/com/gmail/woosay333/onlinebookstore/repository/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package com.gmail.woosay333.onlinebookstore.repository;
-
-import com.gmail.woosay333.onlinebookstore.entity.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepository extends JpaRepository<Book, Long> {
-}
