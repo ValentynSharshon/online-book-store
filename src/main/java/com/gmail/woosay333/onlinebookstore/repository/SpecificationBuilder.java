17b3265db5aba41b87b31ff8e006710bb84e2a1e
Valentyn Sharshon <woosay333@gmail.com>
2024-04-30 21:12:19 +0300

    created SpecificationBuilder interface

--- src/main/java/com/gmail/woosay333/onlinebookstore/repository/SpecificationBuilder.java
+++ src/main/java/com/gmail/woosay333/onlinebookstore/repository/SpecificationBuilder.java
@@ -1,9 +1,8 @@
 package com.gmail.woosay333.onlinebookstore.repository;
 
 import com.gmail.woosay333.onlinebookstore.dto.BookSearchParameters;
-import com.gmail.woosay333.onlinebookstore.entity.Book;
 import org.springframework.data.jpa.domain.Specification;
 
 public interface SpecificationBuilder<T> {
-    Specification<Book> build(BookSearchParameters bookSearchParameters);
+    Specification<T> build(BookSearchParameters bookSearchParameters);
 }
