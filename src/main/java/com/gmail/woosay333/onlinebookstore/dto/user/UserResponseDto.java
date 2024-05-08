4892eada8c2d172030db53076f0215273cc81a46
Valentyn Sharshon <woosay333@gmail.com>
2024-05-08 23:03:33 +0300

    created UserResponseDto dto class

---
+++ src/main/java/com/gmail/woosay333/onlinebookstore/dto/user/UserResponseDto.java
@@ -0,0 +1,12 @@
+package com.gmail.woosay333.onlinebookstore.dto.user;
+
+import lombok.Data;
+
+@Data
+public class UserResponseDto {
+    private Long id;
+    private String email;
+    private String firstName;
+    private String lastName;
+    private String shippingAddress;
+}
