88bd3931cb1fae15fb02d1ecc48e3eb64e1036d4
Valentyn Sharshon <woosay333@gmail.com>
2024-05-09 21:38:31 +0300

    Changed the code regarding the mentor's comments

--- src/main/java/com/gmail/woosay333/onlinebookstore/dto/user/UserResponseDto.java
+++ src/main/java/com/gmail/woosay333/onlinebookstore/dto/user/UserResponseDto.java
@@ -1,12 +1,22 @@
 package com.gmail.woosay333.onlinebookstore.dto.user;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 @Data
 public class UserResponseDto {
+    @Schema(example = "56")
     private Long id;
+
+    @Schema(example = "example@email.com")
     private String email;
+
+    @Schema(example = "Bob")
     private String firstName;
+
+    @Schema(example = "Bobson")
     private String lastName;
+
+    @Schema(example = "37173 Ritchie Parks, Goodwinville, AK 94927, USA")
     private String shippingAddress;
 }
