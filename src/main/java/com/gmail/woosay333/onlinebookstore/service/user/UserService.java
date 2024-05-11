ba018f0b167fc8cbc1d49f4edb75a3bcf29fbb1e
Valentyn Sharshon <woosay333@gmail.com>
2024-05-11 11:37:33 +0300

    Changed the code regarding the mentor's comments

--- src/main/java/com/gmail/woosay333/onlinebookstore/service/user/UserService.java
+++ src/main/java/com/gmail/woosay333/onlinebookstore/service/user/UserService.java
@@ -2,8 +2,7 @@ package com.gmail.woosay333.onlinebookstore.service.user;
 
 import com.gmail.woosay333.onlinebookstore.dto.user.UserRegistrationRequestDto;
 import com.gmail.woosay333.onlinebookstore.dto.user.UserResponseDto;
-import com.gmail.woosay333.onlinebookstore.exception.RegistrationException;
 
 public interface UserService {
-    UserResponseDto register(UserRegistrationRequestDto dto) throws RegistrationException;
+    UserResponseDto register(UserRegistrationRequestDto dto);
 }
