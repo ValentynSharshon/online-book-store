43fa70e6c7e2fa43bdcf8fdcfed8a05a9305449c
Valentyn Sharshon <woosay333@gmail.com>
2024-05-12 23:41:40 +0300

    created UserRepository interface

--- src/main/java/com/gmail/woosay333/onlinebookstore/repository/user/UserRepository.java
+++ src/main/java/com/gmail/woosay333/onlinebookstore/repository/user/UserRepository.java
@@ -1,8 +1,14 @@
 package com.gmail.woosay333.onlinebookstore.repository.user;
 
 import com.gmail.woosay333.onlinebookstore.entity.User;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    @EntityGraph(attributePaths = {"roles"})
     boolean existsUserByEmail(String email);
+
+    @EntityGraph(attributePaths = {"roles"})
+    Optional<User> findUserByEmail(String email);
 }
