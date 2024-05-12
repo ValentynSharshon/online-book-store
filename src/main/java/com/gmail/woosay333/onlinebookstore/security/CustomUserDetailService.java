df996facb4ba871d4572876d67b996e0e6d59878
Valentyn Sharshon <woosay333@gmail.com>
2024-05-12 23:39:45 +0300

    created the implementation for UserDetailsService interface

---
+++ src/main/java/com/gmail/woosay333/onlinebookstore/security/CustomUserDetailService.java
@@ -0,0 +1,21 @@
+package com.gmail.woosay333.onlinebookstore.security;
+
+import com.gmail.woosay333.onlinebookstore.repository.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findUserByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException(
+                        String.format("User with email %s not found", email)));
+    }
+}
