00d4872b6c2f0262a39f52b7fe12e2c99f20e002
Valentyn Sharshon <woosay333@gmail.com>
2024-05-13 23:14:14 +0300

    changed RegistrationException type to the checked

--- src/main/java/com/gmail/woosay333/onlinebookstore/service/user/UserService.java
+++ src/main/java/com/gmail/woosay333/onlinebookstore/service/user/UserService.java
@@ -2,7 +2,8 @@ package com.gmail.woosay333.onlinebookstore.service.user;
 
 import com.gmail.woosay333.onlinebookstore.dto.user.UserRegistrationRequestDto;
 import com.gmail.woosay333.onlinebookstore.dto.user.UserResponseDto;
+import com.gmail.woosay333.onlinebookstore.exception.RegistrationException;
 
 public interface UserService {
-    UserResponseDto register(UserRegistrationRequestDto dto);
+    UserResponseDto register(UserRegistrationRequestDto dto) throws RegistrationException;
 }
