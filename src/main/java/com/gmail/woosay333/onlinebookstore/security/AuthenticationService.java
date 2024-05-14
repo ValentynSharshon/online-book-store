1ab87645a770f808a54c49ba570fad6586bdabe0
Valentyn Sharshon <woosay333@gmail.com>
2024-05-15 00:29:39 +0300

    created AuthenticationService class

---
+++ src/main/java/com/gmail/woosay333/onlinebookstore/security/AuthenticationService.java
@@ -0,0 +1,25 @@
+package com.gmail.woosay333.onlinebookstore.security;
+
+import com.gmail.woosay333.onlinebookstore.dto.user.UserLoginRequestDto;
+import com.gmail.woosay333.onlinebookstore.dto.user.UserLoginResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+    private final JwtUtil jwtUtil;
+    private final AuthenticationManager authenticationManager;
+
+    public UserLoginResponseDto authenticate(UserLoginRequestDto userLoginRequestDto) {
+        final Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        userLoginRequestDto.email(), userLoginRequestDto.password()));
+
+        String generatedToken = jwtUtil.generateToken(authentication.getName());
+        return new UserLoginResponseDto(generatedToken);
+    }
+}
