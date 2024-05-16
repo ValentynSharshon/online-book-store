a60ac56f41ea6fb5fc52800486d736ad003a2e42
Valentyn Sharshon <woosay333@gmail.com>
2024-05-16 15:23:44 +0300

    created CategoryMapper class

---
+++ src/main/java/com/gmail/woosay333/onlinebookstore/mapper/CategoryMapper.java
@@ -0,0 +1,14 @@
+package com.gmail.woosay333.onlinebookstore.mapper;
+
+import com.gmail.woosay333.onlinebookstore.config.MapperConfig;
+import com.gmail.woosay333.onlinebookstore.dto.category.CategoryRequestDto;
+import com.gmail.woosay333.onlinebookstore.dto.category.CategoryResponseDto;
+import com.gmail.woosay333.onlinebookstore.entity.Category;
+import org.mapstruct.Mapper;
+
+@Mapper(config = MapperConfig.class)
+public interface CategoryMapper {
+    CategoryResponseDto toDto(Category category);
+
+    Category toModel(CategoryRequestDto categoryDto);
+}
