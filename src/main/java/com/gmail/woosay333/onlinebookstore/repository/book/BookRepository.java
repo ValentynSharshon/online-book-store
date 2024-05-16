7c2727217656f64c48a9bb561663cc054691b1b9
Valentyn Sharshon <woosay333@gmail.com>
2024-05-16 15:25:15 +0300

    added findAllByCategoryId, findAllWithCategories, findByIdWithCategories methods

--- src/main/java/com/gmail/woosay333/onlinebookstore/repository/book/BookRepository.java
+++ src/main/java/com/gmail/woosay333/onlinebookstore/repository/book/BookRepository.java
@@ -1,9 +1,22 @@
 package com.gmail.woosay333.onlinebookstore.repository.book;
 
 import com.gmail.woosay333.onlinebookstore.entity.Book;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 
 public interface BookRepository extends JpaRepository<Book, Long>, JpaSpecificationExecutor<Book> {
     boolean existsBookByIsbn(String isbn);
+
+    @Query("FROM Book b LEFT JOIN FETCH b.categories c WHERE c.id = :categoryId")
+    List<Book> findAllByCategoryId(Long categoryId, Pageable pageable);
+
+    @Query("FROM Book b LEFT JOIN FETCH b.categories")
+    List<Book> findAllWithCategories(Pageable pageable);
+
+    @Query("FROM Book b LEFT JOIN FETCH b.categories WHERE b.id = :id")
+    Optional<Book> findByIdWithCategories(Long id);
 }
