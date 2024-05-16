2e71dc9e1b6558137ff1fdf55d767a741d4729c6
Valentyn Sharshon <woosay333@gmail.com>
2024-05-16 15:22:41 +0300

    created Category entity class

--- src/main/java/com/gmail/woosay333/onlinebookstore/entity/Category.java
+++ src/main/java/com/gmail/woosay333/onlinebookstore/entity/Category.java
@@ -6,13 +6,9 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
+import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.experimental.Accessors;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.SQLRestriction;
 
@@ -21,12 +17,8 @@ import org.hibernate.annotations.SQLRestriction;
 @SQLDelete(sql = "UPDATE categories SET is_deleted = true WHERE id=?")
 @SQLRestriction("is_deleted = false")
 @NoArgsConstructor
-@AllArgsConstructor
-@Getter
-@Setter
-@EqualsAndHashCode
-@ToString
-@Builder
+@Data
+@Accessors(chain = true)
 public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -38,6 +30,9 @@ public class Category {
     private String description;
 
     @Column(nullable = false)
-    @Builder.Default
     private boolean isDeleted = false;
+
+    public Category(Long id) {
+        this.id = id;
+    }
 }
