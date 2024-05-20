20da46a469b3f4d86af2012aea5fdb27e419fb6a
Valentyn Sharshon <woosay333@gmail.com>
2024-05-21 01:10:37 +0300

    Added getAuthenticatedUser() method

--- src/main/java/com/gmail/woosay333/onlinebookstore/service/user/UserService.java
+++ src/main/java/com/gmail/woosay333/onlinebookstore/service/user/UserService.java
@@ -2,8 +2,11 @@ package com.gmail.woosay333.onlinebookstore.service.user;
 
 import com.gmail.woosay333.onlinebookstore.dto.user.UserRegistrationRequestDto;
 import com.gmail.woosay333.onlinebookstore.dto.user.UserResponseDto;
+import com.gmail.woosay333.onlinebookstore.entity.User;
 import com.gmail.woosay333.onlinebookstore.exception.RegistrationException;
 
 public interface UserService {
     UserResponseDto register(UserRegistrationRequestDto dto) throws RegistrationException;
+
+    User getAuthenticatedUser();
 }
