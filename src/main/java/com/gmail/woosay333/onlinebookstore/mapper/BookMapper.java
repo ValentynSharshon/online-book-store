2167d138c459af30bb24ac3eefd9eb0779065ba3
Valentyn Sharshon <woosay333@gmail.com>
2024-05-21 01:09:08 +0300

    Modified BookMapper class

--- src/main/java/com/gmail/woosay333/onlinebookstore/mapper/BookMapper.java
+++ src/main/java/com/gmail/woosay333/onlinebookstore/mapper/BookMapper.java
@@ -6,35 +6,54 @@ import com.gmail.woosay333.onlinebookstore.dto.book.BookDtoWithoutCategoryIds;
 import com.gmail.woosay333.onlinebookstore.dto.book.BookRequestDto;
 import com.gmail.woosay333.onlinebookstore.entity.Book;
 import com.gmail.woosay333.onlinebookstore.entity.Category;
+import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
+import org.mapstruct.AfterMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 import org.mapstruct.Named;
 import org.mapstruct.ReportingPolicy;
 
 @Mapper(config = MapperConfig.class, unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface BookMapper {
-    @Mapping(target = "categoryIds", source = "categories", qualifiedByName = "setCategoryIds")
+    @Mapping(target = "categoryIds", source = "categories")
     BookDto toDto(Book book);
 
-    @Named("setCategoryIds")
-    default Set<Long> setCategoryIds(Set<Category> categories) {
+    @AfterMapping
+    default void setCategoryIds(@MappingTarget BookDto bookDto, Book book) {
+        if (book.getCategories() != null) {
+            bookDto.setCategoryIds(book.getCategories().stream()
+                    .map(Category::getId)
+                    .collect(Collectors.toSet()));
+        }
+    }
+
+    @Mapping(target = "categories", source = "categoryIds")
+    Book toEntity(BookRequestDto bookRequestDto);
+
+    default Set<Category> mapCategorySet(Set<Long> categoryIds) {
+        return categoryIds.stream()
+                .map(Category::new)
+                .collect(Collectors.toSet());
+    }
+
+    default Set<Long> mapCategoryToIds(Set<Category> categories) {
         return categories.stream()
                 .map(Category::getId)
                 .collect(Collectors.toSet());
     }
 
-    @Mapping(target = "id", ignore = true)
-    @Mapping(target = "categories", source = "categoryIds", qualifiedByName = "getCategoryById")
-    Book toModel(BookRequestDto bookRequestDto);
-
-    @Named("getCategoryById")
-    default Set<Category> getCategoryById(Set<Long> categoryIds) {
-        return categoryIds.stream()
-                .map(Category::new)
-                .collect(Collectors.toSet());
+    @Named("bookFromId")
+    default Book bookFromId(Long id) {
+        return Optional
+                .ofNullable(id)
+                .map(Book::new)
+                .orElse(null);
     }
+
+    Book toModel(BookRequestDto bookRequestDto);
 
     BookDtoWithoutCategoryIds toDtoWithoutCategoryIds(Book book);
 }
