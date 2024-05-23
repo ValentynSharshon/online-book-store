7e795781eb4cbcd9bb44908066788825e9f5ef80
Valentyn Sharshon <woosay333@gmail.com>
2024-05-23 16:07:43 +0300

    Created OrderItemMapper class

---
+++ src/main/java/com/gmail/woosay333/onlinebookstore/mapper/OrderItemMapper.java
@@ -0,0 +1,13 @@
+package com.gmail.woosay333.onlinebookstore.mapper;
+
+import com.gmail.woosay333.onlinebookstore.config.MapperConfig;
+import com.gmail.woosay333.onlinebookstore.dto.orderitem.OrderItemResponseDto;
+import com.gmail.woosay333.onlinebookstore.entity.OrderItem;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(config = MapperConfig.class)
+public interface OrderItemMapper {
+    @Mapping(target = "bookId", source = "book.id")
+    OrderItemResponseDto toDto(OrderItem orderItem);
+}
