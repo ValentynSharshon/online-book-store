1832f5efff464a1d9ce2c0425cfc2f9443f16f71
Valentyn Sharshon <woosay333@gmail.com>
2024-05-23 16:08:18 +0300

    Created OrderItemRepository interface

---
+++ src/main/java/com/gmail/woosay333/onlinebookstore/repository/orderitem/OrderItemRepository.java
@@ -0,0 +1,12 @@
+package com.gmail.woosay333.onlinebookstore.repository.orderitem;
+
+import com.gmail.woosay333.onlinebookstore.entity.OrderItem;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+    List<OrderItem> findByOrderId(Long orderId);
+
+    Optional<OrderItem> findByIdAndOrderId(Long itemId, Long orderId);
+}
