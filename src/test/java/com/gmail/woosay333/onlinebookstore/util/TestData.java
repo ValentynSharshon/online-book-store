cd59afb3e7c3512f0bb7b8a56036b329e965e0ec
Valentyn Sharshon <woosay333@gmail.com>
2024-06-10 15:37:31 +0300

    Added data for tests

---
+++ src/test/java/com/gmail/woosay333/onlinebookstore/util/TestData.java
@@ -0,0 +1,13 @@
+package com.gmail.woosay333.onlinebookstore.util;
+
+import java.math.BigDecimal;
+import java.util.Set;
+
+public class TestData {
+    public static final String VALID_BOOK_TITLE_HOBBIT = "The Hobbit";
+    public static final String VALID_BOOK_AUTHOR_TOLKIEN = "J.R.R. Tolkien";
+    public static final String VALID_BOOK_ISBN_HOBBIT = "9780547928227";
+    public static final Set<Long> VALID_BOOK_CATEGORY_IDS_HOBBIT = Set.of(2L);
+    public static final BigDecimal VALID_BOOK_PRICE_HOBBIT = BigDecimal.valueOf(14.99);
+    public static final Long VALID_BOOK_ID = 1L;
+}
