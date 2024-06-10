1e7b840631ea5d5dca3abd8e05563e492e8b9d84
Valentyn Sharshon <woosay333@gmail.com>
2024-06-10 23:29:55 +0300

    Created BookRepositoryTest class

---
+++ src/test/java/com/gmail/woosay333/onlinebookstore/repository/book/BookRepositoryTest.java
@@ -0,0 +1,14 @@
+package com.gmail.woosay333.onlinebookstore.repository.book;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class BookRepositoryTest {
+    @Autowired
+    private BookRepository bookRepository;
+
+
+}
