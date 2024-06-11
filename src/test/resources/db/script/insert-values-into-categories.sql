63f3192ed65cdc033c6cc4985582234767313fc5
Valentyn Sharshon <woosay333@gmail.com>
2024-06-11 22:54:03 +0300

    Created sql scripts for tests

---
+++ src/test/resources/db/script/insert-values-into-categories.sql
@@ -0,0 +1,2 @@
+INSERT INTO categories (id, name, description) VALUES (1, 'Fantasy', 'Fantasy description');
+INSERT INTO categories (id, name, description) VALUES (2, 'Novel', 'Novel description');
