89232ce233c3d2ac2e5a022d7657131fd6f4274a
Valentyn Sharshon <woosay333@gmail.com>
2024-06-13 20:37:37 +0300

    Added beforeEach and afterEach methods

--- src/test/java/com/gmail/woosay333/onlinebookstore/repository/book/BookRepositoryTest.java
+++ src/test/java/com/gmail/woosay333/onlinebookstore/repository/book/BookRepositoryTest.java
@@ -13,18 +13,24 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import com.gmail.woosay333.onlinebookstore.entity.Book;
+import java.sql.Connection;
 import java.util.List;
 import java.util.Optional;
+import javax.sql.DataSource;
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
-import org.springframework.test.context.jdbc.Sql;
+import org.springframework.jdbc.datasource.init.ScriptUtils;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
@@ -32,15 +38,34 @@ class BookRepositoryTest {
     @Autowired
     private BookRepository bookRepository;
 
+    @BeforeEach
+    @SneakyThrows
+    public void beforeEach(@Autowired DataSource dataSource) {
+        afterEach(dataSource);
+        try (Connection connection = dataSource.getConnection()) {
+            connection.setAutoCommit(true);
+            ScriptUtils.executeSqlScript(connection,
+                    new ClassPathResource(INSERT_BOOKS_SQL));
+            ScriptUtils.executeSqlScript(connection,
+                    new ClassPathResource(INSERT_CATEGORIES_SQL));
+            ScriptUtils.executeSqlScript(connection,
+                    new ClassPathResource(INSERT_BOOKS_CATEGORIES_SQL));
+        }
+    }
+
+    @AfterEach
+    @SneakyThrows
+    public void afterEach(@Autowired DataSource dataSource) {
+        try (Connection connection = dataSource.getConnection()) {
+            connection.setAutoCommit(true);
+            ScriptUtils.executeSqlScript(connection, new ClassPathResource(DELETE_VALUES_SQL));
+        }
+    }
+
     @Test
     @DisplayName("""
             Find book by existing ISBN, return optional of book by ISBN
             """)
-    @Sql(scripts = {DELETE_VALUES_SQL,
-            INSERT_BOOKS_SQL,
-            INSERT_CATEGORIES_SQL,
-            INSERT_BOOKS_CATEGORIES_SQL},
-            executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
     void findByIsbn_ExistingBookIsbn_ReturnOptionalOfBook() {
         Optional<Book> actual = bookRepository.findByIsbn(VALID_BOOK_ISBN_HOBBIT);
 
@@ -51,11 +76,6 @@ class BookRepositoryTest {
     @DisplayName("""
             Find book by non existing ISBN, return empty optional of book by ISBN
             """)
-    @Sql(scripts = {DELETE_VALUES_SQL,
-            INSERT_BOOKS_SQL,
-            INSERT_CATEGORIES_SQL,
-            INSERT_BOOKS_CATEGORIES_SQL},
-            executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
     void findByIsbn_NonExistingBookIsbn_ReturnEmptyOptionalOfBook() {
         Optional<Book> actual = bookRepository.findByIsbn(INVALID_BOOK_ISBN);
 
@@ -67,11 +87,6 @@ class BookRepositoryTest {
             Find book with categories by existing ID, 
             return optional of book with set of categories
             """)
-    @Sql(scripts = {DELETE_VALUES_SQL,
-            INSERT_BOOKS_SQL,
-            INSERT_CATEGORIES_SQL,
-            INSERT_BOOKS_CATEGORIES_SQL},
-            executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
     void findByIdWithCategories_FindingBookByExistingId_ReturnOptionalOfBook() {
         Optional<Book> actual = bookRepository.findByIdWithCategories(VALID_BOOK_ID);
 
@@ -83,11 +98,6 @@ class BookRepositoryTest {
     @DisplayName("""
             Find book with categories by non existing ID, return empty optional
             """)
-    @Sql(scripts = {DELETE_VALUES_SQL,
-            INSERT_BOOKS_SQL,
-            INSERT_CATEGORIES_SQL,
-            INSERT_BOOKS_CATEGORIES_SQL},
-            executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
     void findByIdWithCategories_FindingBookByNonExistingId_ReturnEmptyOptional() {
         Optional<Book> actual = bookRepository.findByIdWithCategories(INVALID_BOOK_ID);
 
@@ -98,11 +108,6 @@ class BookRepositoryTest {
     @DisplayName("""
             Find all books with categories by params, return page of books
             """)
-    @Sql(scripts = {DELETE_VALUES_SQL,
-            INSERT_BOOKS_SQL,
-            INSERT_CATEGORIES_SQL,
-            INSERT_BOOKS_CATEGORIES_SQL},
-            executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
     void findAllByParams_ReturnAllBooksByParams() {
         Specification<Book> specification = Specification.where(null);
         Pageable pageable = Pageable.unpaged();
@@ -120,11 +125,6 @@ class BookRepositoryTest {
     @DisplayName("""
             Find all books with categories, returns list of books
             """)
-    @Sql(scripts = {DELETE_VALUES_SQL,
-            INSERT_BOOKS_SQL,
-            INSERT_CATEGORIES_SQL,
-            INSERT_BOOKS_CATEGORIES_SQL},
-            executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
     void findAllBooks_GetAllBooks_ReturnAllBooks() {
         Pageable pageable = Pageable.unpaged();
 
@@ -141,11 +141,6 @@ class BookRepositoryTest {
     @DisplayName("""
             Find all books witch belongs to category ID, returns list of books
             """)
-    @Sql(scripts = {DELETE_VALUES_SQL,
-            INSERT_BOOKS_SQL,
-            INSERT_CATEGORIES_SQL,
-            INSERT_BOOKS_CATEGORIES_SQL},
-            executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
     void findAllByCategoryId_ExistCategoryId_ReturnAllBooksByCategoryId() {
         Pageable pageable = Pageable.unpaged();
         Long fantasyId = 1L;
@@ -168,11 +163,6 @@ class BookRepositoryTest {
     @DisplayName("""
             Find all books by ID and ISBN, returns list of books
             """)
-    @Sql(scripts = {DELETE_VALUES_SQL,
-            INSERT_BOOKS_SQL,
-            INSERT_CATEGORIES_SQL,
-            INSERT_BOOKS_CATEGORIES_SQL},
-            executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
     void findAllByIdOrIsbn_GetAllBooksByIdAndIsbn_ReturnsAllBooksByIdAndIsbn() {
         final List<Book> actualValidIdValidIsbn = bookRepository
                 .findAllByIdOrIsbn(VALID_BOOK_ID, VALID_BOOK_ISBN_HOBBIT);
