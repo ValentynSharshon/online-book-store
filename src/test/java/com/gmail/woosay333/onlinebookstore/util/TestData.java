aae9a77050c7fd09765156af7579ec0af4f6d735
Valentyn Sharshon <woosay333@gmail.com>
2024-06-13 23:11:27 +0300

    Added some test data

--- src/test/java/com/gmail/woosay333/onlinebookstore/util/TestData.java
+++ src/test/java/com/gmail/woosay333/onlinebookstore/util/TestData.java
@@ -8,6 +8,7 @@ public class TestData {
     public static final String VALID_BOOK_AUTHOR_TOLKIEN = "J.R.R. Tolkien";
     public static final String VALID_BOOK_ISBN_HOBBIT = "9780547928227";
     public static final String INVALID_BOOK_ISBN = "9780547928123";
+    public static final String EXIST_BOOK_ISBN = "9780439064873";
     public static final Set<Long> VALID_BOOK_CATEGORY_IDS_HOBBIT = Set.of(2L);
     public static final BigDecimal VALID_BOOK_PRICE_HOBBIT = BigDecimal.valueOf(14.99);
     public static final Long VALID_BOOK_ID = 1L;
@@ -21,4 +22,11 @@ public class TestData {
             "db/script/insert-values-into-categories.sql";
     public static final String INSERT_BOOKS_CATEGORIES_SQL =
             "db/script/insert-values-into-books-categories.sql";
+
+    public static final String VALID_BOOK_TITLE_HARRY_POTTER =
+            "Harry Potter and the Order of the Phoenix";
+    public static final String VALID_BOOK_AUTHOR_ROWLING = "J.K. Rowling";
+    public static final String VALID_BOOK_ISBN_HARRY_POTTER = "9780439358071";
+    public static final Set<Long> VALID_BOOK_CATEGORY_IDS_HARRY_POTTER = Set.of(2L);
+    public static final BigDecimal VALID_BOOK_PRICE_HARRY_POTTER = BigDecimal.valueOf(12.99);
 }
